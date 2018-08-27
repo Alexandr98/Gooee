5ae6d707e58726ff38ef5644b64a8397ec9e40a7
Alexandr Pryimak <alexandrpryimak@MacBook-Pro-MacOS.local>
2018-08-28 00:32:59 +0300

    setup project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SharedComponent } from './shared/shared.component';
+import { LandingComponent } from './shared/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SharedComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
