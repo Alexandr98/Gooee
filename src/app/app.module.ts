436cac3cc5479febb6743359d05d14af62085cd9
Alexandr Pryimak <alexandrpryimak@MacBook-Pro-MacOS.local>
2018-08-30 00:53:40 +0300

    add background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { SharedComponent } from './shared/shared.component';
 import { LandingComponent } from './shared/landing/landing.component';
 
 @NgModule({
@@ -12,7 +11,6 @@ import { LandingComponent } from './shared/landing/landing.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SharedComponent,
     LandingComponent
   ],
   imports: [
