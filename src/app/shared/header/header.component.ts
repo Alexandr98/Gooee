8896c93bd9ea51f3f05e771dff9a68a40ba092ea
Alexandr Pryimak <alexandrpryimak@MacBook-Pro-MacOS.local>
2018-09-08 18:27:34 +0300

    create header component with animation

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, AfterViewInit } from '@angular/core';
+import { WOW } from 'wowjs/dist/wow.min';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HeaderComponent implements AfterViewInit {
+  public ngAfterViewInit() {
+    new WOW().init();
   }
-
 }
